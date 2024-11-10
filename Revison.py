39ae519b6f0c095f54ff387951c65caac4ea984b
IshiJads <ishita.jadolia@aics.espritscholen.nl>
2024-11-10 11:59:23 +0100

    Create Revison.py

---
+++ Revison.py
@@ -0,0 +1,10 @@
+print ("Ishita")
+My_name=input ("What is your name?")
+
+My_age=int (input ("How old are you?"))
+
+if My_age>=18:
+    print ("Allowed to take a drivers licence.")
+
+else:
+    print ("Not allowed to take a drivers licence")
